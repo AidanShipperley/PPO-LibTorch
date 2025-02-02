4675afaafc2595b10d61e052cf175bb5407c4ee8
AidanShipperley <70974869+AidanShipperley@users.noreply.github.com>
2025-02-02 04:16:05 -0500

    Updated cudnn location possibilities for Windows

--- cmake/FindCUDNN.cmake
+++ cmake/FindCUDNN.cmake
@@ -1,3 +1,8 @@
+# This file is a modified version of the FindCUDNN.cmake from OpenCV (https://github.com/opencv/opencv/blob/a03b81316782ae30038b288fd3568993fa1e3538/cmake/FindCUDNN.cmake) which uses the Apache License v2.0
+# This is compatible with the GPL v3.0 license used for this project according to Apache: https://www.apache.org/licenses/GPL-compatibility.html
+
+# The modifications basically remove the FindCUDA.cmake dependency in OpenCV's version, which is quite large and mainly needed for older CMake versions
+
 #[=======================================================================[.rst:
 FindCUDNN
 ---------
@@ -42,55 +47,84 @@ if(WIN32)
     
     message(STATUS "FindCUDNN: CUDNN_ROOT_DIR = ${CUDNN_ROOT_DIR}")
     
-    # Find the latest cuDNN version installed
-    file(GLOB _CUDNN_VERSIONS "${CUDNN_ROOT_DIR}/v*")
-    if(_CUDNN_VERSIONS)
-        # Get latest version
-        list(SORT _CUDNN_VERSIONS)
-        list(GET _CUDNN_VERSIONS -1 _LATEST_CUDNN)
-        message(STATUS "FindCUDNN: Found latest cuDNN at ${_LATEST_CUDNN}")
+    # Function to search for cuDNN in a specific directory structure
+    function(find_cudnn_in_directory BASE_PATH VERSION_PATH CUDA_VERSION)
+        set(_LIB_PATHS
+            # GUI Install paths
+            "${BASE_PATH}/${VERSION_PATH}/lib/${CUDA_VERSION}/x64"
+            "${BASE_PATH}/${VERSION_PATH}/lib/x64"
+            # ZIP install paths
+            "${BASE_PATH}/lib/x64"
+            "${BASE_PATH}/lib"
+        )
         
-        # Look for CUDA version-specific paths
-        set(_CUDA_VERSION "${CUDAToolkit_VERSION_MAJOR}.${CUDAToolkit_VERSION_MINOR}")
-        set(_LIB_PATH "${_LATEST_CUDNN}/lib/${_CUDA_VERSION}/x64")
-        set(_INCLUDE_PATH "${_LATEST_CUDNN}/include/${_CUDA_VERSION}")
+        set(_INCLUDE_PATHS
+            # GUI Install paths
+            "${BASE_PATH}/${VERSION_PATH}/include/${CUDA_VERSION}"
+            "${BASE_PATH}/${VERSION_PATH}/include"
+            # ZIP install paths
+            "${BASE_PATH}/include"
+        )
         
-        # Find development files
+        set(_BIN_PATHS
+            # GUI Install paths
+            "${BASE_PATH}/${VERSION_PATH}/bin/${CUDA_VERSION}"
+            "${BASE_PATH}/${VERSION_PATH}/bin"
+            # ZIP install paths
+            "${BASE_PATH}/bin"
+        )
+        
+        # Find Library
         find_library(CUDNN_LIBRARY
             NAMES cudnn
-            HINTS 
-                "${_LIB_PATH}"
-                "${CUDNN_ROOT_DIR}/lib/x64"  # Allow for non-versioned paths too
+            HINTS ${_LIB_PATHS}
             NO_DEFAULT_PATH
         )
         
+        # Find Include
         find_path(CUDNN_INCLUDE_DIR
             cudnn.h
-            HINTS 
-                "${_INCLUDE_PATH}"
-                "${CUDNN_ROOT_DIR}/include"  # Allow for non-versioned paths too
+            HINTS ${_INCLUDE_PATHS}
             NO_DEFAULT_PATH
         )
-
         
-        if(CUDNN_LIBRARY AND CUDNN_INCLUDE_DIR)
-            message(STATUS "FindCUDNN: Found cuDNN development files:")
-            message(STATUS "  Include: ${CUDNN_INCLUDE_DIR}")
-            message(STATUS "  Library: ${CUDNN_LIBRARY}")
-            
-            # Look for runtime DLLs in LibTorch
-            if(LIBTORCH_PATH)
-                file(GLOB CUDNN_DLLS 
-                    "${LIBTORCH_PATH}/lib/cudnn64_*.dll"
-                    "${LIBTORCH_PATH}/lib/cudnn_*64_*.dll"
-                )
-                if(CUDNN_DLLS)
-                    message(STATUS "FindCUDNN: Found cuDNN runtime DLLs in LibTorch:")
-                    message(STATUS "  DLLs: ${CUDNN_DLLS}")
-                    get_filename_component(CUDNN_DLL_DIR "${LIBTORCH_PATH}/lib" ABSOLUTE)
+        # Find DLLs
+        if(EXISTS "${CUDNN_LIBRARY}")
+            foreach(_bin_path ${_BIN_PATHS})
+                file(GLOB _CUDNN_DLLS "${_bin_path}/cudnn*.dll")
+                if(_CUDNN_DLLS)
+                    get_filename_component(CUDNN_DLL_DIR "${_bin_path}" ABSOLUTE)
+                    set(CUDNN_DLL_DIR "${CUDNN_DLL_DIR}" PARENT_SCOPE)
+                    break()
                 endif()
-            endif()
+            endforeach()
         endif()
+        
+        # Propagate results to parent scope
+        set(CUDNN_LIBRARY "${CUDNN_LIBRARY}" PARENT_SCOPE)
+        set(CUDNN_INCLUDE_DIR "${CUDNN_INCLUDE_DIR}" PARENT_SCOPE)
+    endfunction()
+
+    # Get CUDA version for version-specific paths
+    set(_CUDA_VERSION "${CUDAToolkit_VERSION_MAJOR}.${CUDAToolkit_VERSION_MINOR}")
+    
+    # First check for GUI installation with version directory
+    file(GLOB _VERSION_DIRS "${CUDNN_ROOT_DIR}/v*")
+    if(_VERSION_DIRS)
+        # Get latest version if multiple exist
+        list(SORT _VERSION_DIRS)
+        list(GET _VERSION_DIRS -1 _LATEST_VERSION_DIR)
+        get_filename_component(_VERSION_NAME "${_LATEST_VERSION_DIR}" NAME)
+        message(STATUS "FindCUDNN: Found version directory: ${_VERSION_NAME}")
+        
+        # Search in versioned directory
+        find_cudnn_in_directory("${CUDNN_ROOT_DIR}" "${_VERSION_NAME}" "${_CUDA_VERSION}")
+    endif()
+    
+    # If not found in version directory, try direct installation layout
+    if(NOT CUDNN_LIBRARY OR NOT CUDNN_INCLUDE_DIR)
+        message(STATUS "FindCUDNN: Trying direct installation layout")
+        find_cudnn_in_directory("${CUDNN_ROOT_DIR}" "" "")
     endif()
 else()
     # On Linux/Mac, look for the library in standard locations
@@ -126,7 +160,6 @@ if(CUDNN_INCLUDE_DIR)
     message(STATUS "Found cuDNN version: ${CUDNN_VERSION}")
 endif()
 
-# Handle the find_package arguments and set the CUDNN_FOUND variable
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(CUDNN
     FOUND_VAR CUDNN_FOUND
@@ -139,7 +172,12 @@ find_package_handle_standard_args(CUDNN
 if(CUDNN_FOUND)
     set(CUDNN_LIBRARIES ${CUDNN_LIBRARY})
     set(CUDNN_INCLUDE_DIRS ${CUDNN_INCLUDE_DIR})
-    message(STATUS "Found cuDNN: ${CUDNN_LIBRARY}")
+    message(STATUS "Found cuDNN:")
+    message(STATUS "  Library: ${CUDNN_LIBRARY}")
+    message(STATUS "  Include: ${CUDNN_INCLUDE_DIR}")
+    if(WIN32 AND CUDNN_DLL_DIR)
+        message(STATUS "  DLLs: ${CUDNN_DLL_DIR}")
+    endif()
     
     # Force enable CAFFE2_USE_CUDNN
     set(CAFFE2_USE_CUDNN TRUE CACHE BOOL "Use cuDNN" FORCE)
