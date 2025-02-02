5a9f9ccac3fd09c8638e63339ba45cc9b2acd884
AidanShipperley <70974869+AidanShipperley@users.noreply.github.com>
2025-02-02 03:11:18 -0500

    Remove helper cpp file accidentally in build

--- cmake/check_cudnn_available.cpp
+++ cmake/check_cudnn_available.cpp
@@ -1,11 +1,15 @@
 #include <torch/cuda.h>
+#include <iostream>
 
 // In your initialization code:
-if (torch::cuda::is_available()) {
-    if (torch::cuda::cudnn_is_available()) {
-        std::cout << "cuDNN is available" << std::endl;
+int main() {
+    if (torch::cuda::is_available()) {
+        if (torch::cuda::cudnn_is_available()) {
+            std::cout << "cuDNN is available" << std::endl;
+        }
+        else {
+            std::cout << "cuDNN is NOT available" << std::endl;
+        }
     }
-    else {
-        std::cout << "cuDNN is NOT available" << std::endl;
-    }
+    return 0;
 }
